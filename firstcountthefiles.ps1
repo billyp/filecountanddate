8ceb618ec748722854e4e8b88f002a6a338cc038
Billy Poggi <billy.poggi@gmail.com>
2016-11-07 21:05:10 -0500

    organized my thoughts more

--- firstcountthefiles.ps1
+++ firstcountthefiles.ps1
@@ -1,16 +1,37 @@
-# will ignore lines in .bat files
-
+# will ignore lines in .ps1 files
+# clear screen
 cls
 
 #sets the script path from the directory in which the script is running from :)
-$scriptpath = $PSScriptRoot
+$PSScriptRoot = 'C:\Users\billy\Google Drive\Projects'
 
-echo ""
-echo "This Script will run in this directory"
+# STEP ONE get into the directory
+cd 'C:\Users\billy\Google Drive\Projects'
+
+# Get-ChildItem -Path 'C:\Users\billy\Google Drive\Projects' -Filter *.CR2 -Recurse -ErrorAction SilentlyContinue -Force
+# Write-Host (Get-ChildItem -dir)
+Write-Host (Get-ChildItem -dir -Recurse | ?{ $_.PSIsContainer } | Select-Object FullName)
+
+
+
+echo "this is the folder you're working in"
 Write-Host $PSScriptRoot
 echo ""
 echo ""
-# nothing else #smiles
+Write-Host ( Get-ChildItem $PSScriptRoot -Recurse -force *.CR2 | Measure-Object ).Count;
+
+echo "the list of Reno Senior Pics"
+# Write-Host ( Get-ChildItem 'C:\Users\billy\Google Drive\Jobs\2016-10-29 Kaitlyn Reny Senior Portraits\2016-10-29' | Measure-Object ).Count;
+Get-ChildItem 'C:\Users\billy\Google Drive\Jobs\2016-10-29 Kaitlyn Reny Senior Portraits\2016-10-29' | Measure-Object
+
+echo "Amount of .CR2 files in " $PSScriptRoot
+Get-ChildItem -Recurse -force | Measure-Object
+
+# return to the file directory for the script
+cd 'C:\Users\billy\Dropbox\Github\filecountanddate\'
 
 
-# # Write-Host ( Get-ChildItem 'C:\Users\billy\Google Drive\Jobs\2016-10-29 Kaitlyn Reny Senior Portraits\2016-10-29' | Measure-Object ).Count;
+# write that directory to file
+# count all files *.CR2
+# apend that count to the file
+# get up a directory
